a61ecf2a3202c189f840e3a690243eeff5e56e23
Julia <julia.melgare@acad.pucrs.br>
2026-03-11 17:29:10 -0300

    create fixation component to be added to game objects

---
+++ Assets/Scripts/Face/Vision/FixationComponent.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixationComponent : MonoBehaviour
+{
+    public Vector3 localPoint;
+    public float imageSaliencyScore;
+    public float motionSaliencyScore;
+}
