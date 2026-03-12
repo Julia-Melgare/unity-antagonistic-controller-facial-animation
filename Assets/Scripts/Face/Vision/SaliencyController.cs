55376c47f1eef9458970cfef2ac9ad7aa78b1888
Julia <julia.melgare@acad.pucrs.br>
2026-03-12 17:16:32 -0300

    preparing for implementation

--- Assets/Scripts/Face/Vision/SaliencyController.cs
+++ Assets/Scripts/Face/Vision/SaliencyController.cs
@@ -159,23 +159,31 @@ public class SaliencyController : MonoBehaviour
         {
             Ray ray = auxiliaryAgentCamera.ScreenPointToRay(screenPoint.Key);
             RaycastHit[] hits = Physics.RaycastAll(ray, Mathf.Infinity, scanLayerMask);
-            foreach (RaycastHit hit in hits)
+            if (hits.Length > 0)
             {
-                GameObject raycastObj = hit.collider.gameObject;
-
-                if (hit.collider.GetType() == typeof(TerrainCollider))
+                foreach (RaycastHit hit in hits)
                 {
-                    //We need to look at that specific point on the terrain instead
-                    GameObject terrainPoint = new GameObject("TerrainPoint", typeof(SelfDestruct));
-                    terrainPoint.transform.position = hit.point;
-                    raycastObj = terrainPoint;
+                    GameObject raycastObj = hit.collider.gameObject;
+
+                    if (hit.collider.GetType() == typeof(TerrainCollider))
+                    {
+                        //We need to look at that specific point on the terrain instead
+                        GameObject terrainPoint = new GameObject("TerrainPoint", typeof(SelfDestruct));
+                        terrainPoint.transform.position = hit.point;
+                        raycastObj = terrainPoint;
+                    }
+
+                    Vector3 hitLocalPos = raycastObj.transform.InverseTransformPoint(hit.point);
+                    FixationObject fixationObject = new FixationObject(raycastObj, hitLocalPos);
+
+                    salientObjectsDict.TryAdd(fixationObject, screenPoint.Value);
                 }
-
-                Vector3 hitLocalPos = raycastObj.transform.InverseTransformPoint(hit.point);
-                FixationObject fixationObject = new FixationObject(raycastObj, hitLocalPos);
-
-                salientObjectsDict.TryAdd(fixationObject, screenPoint.Value);
-            }                          
+            }
+            else
+            {
+                // create fixation from the raycast direction
+            }
+                                      
         }
         salientObjects = new List<FixationObject>(salientObjectsDict.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value).Keys);
         if (debugSaliencyRaycast)
